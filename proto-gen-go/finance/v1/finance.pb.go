418fce51e2e850e5c601224768feff2c5c426d5d
fly <fly@ogmail.org>
2026-07-22 13:49:36 +0500

    转换币种

--- proto-gen-go/finance/v1/finance.pb.go
+++ proto-gen-go/finance/v1/finance.pb.go
@@ -817,21 +817,23 @@ func (WithdrawLimitType) EnumDescriptor() ([]byte, []int) {
 }
 
 type PayInReq struct {
-	state              protoimpl.MessageState `protogen:"open.v1"`
-	UserId             int64                  `protobuf:"varint,1,opt,name=user_id,json=userId,proto3" json:"user_id,omitempty"`                                       // 用户Id
-	OrderId            string                 `protobuf:"bytes,2,opt,name=order_id,json=orderId,proto3" json:"order_id,omitempty"`                                     // 订单Id
-	OrderNumber        string                 `protobuf:"bytes,3,opt,name=order_number,json=orderNumber,proto3" json:"order_number,omitempty"`                         // 订单编号
-	Amount             int64                  `protobuf:"varint,4,opt,name=amount,proto3" json:"amount,omitempty"`                                                     // 金额
-	Phone              string                 `protobuf:"bytes,5,opt,name=phone,proto3" json:"phone,omitempty"`                                                        // 手机号
-	RealName           string                 `protobuf:"bytes,6,opt,name=real_name,json=realName,proto3" json:"real_name,omitempty"`                                  // 姓名
-	RechargeCategoryId int64                  `protobuf:"varint,7,opt,name=recharge_category_id,json=rechargeCategoryId,proto3" json:"recharge_category_id,omitempty"` // 充值大类Id
-	RechargeMerchantId int64                  `protobuf:"varint,8,opt,name=recharge_merchant_id,json=rechargeMerchantId,proto3" json:"recharge_merchant_id,omitempty"` // 代收商户Id
-	CurrencyCode       string                 `protobuf:"bytes,9,opt,name=currency_code,json=currencyCode,proto3" json:"currency_code,omitempty"`                      // 币种
-	RechargeChannelId  int64                  `protobuf:"varint,10,opt,name=recharge_channel_id,json=rechargeChannelId,proto3" json:"recharge_channel_id,omitempty"`   // 充值通道id
-	Extra              string                 `protobuf:"bytes,11,opt,name=extra,proto3" json:"extra,omitempty"`                                                       // 扩展字段
-	Account            string                 `protobuf:"bytes,12,opt,name=account,proto3" json:"account,omitempty"`                                                   // 用户付款帐号（选填）
-	unknownFields      protoimpl.UnknownFields
-	sizeCache          protoimpl.SizeCache
+	state               protoimpl.MessageState `protogen:"open.v1"`
+	UserId              int64                  `protobuf:"varint,1,opt,name=user_id,json=userId,proto3" json:"user_id,omitempty"`                                          // 用户Id
+	OrderId             string                 `protobuf:"bytes,2,opt,name=order_id,json=orderId,proto3" json:"order_id,omitempty"`                                        // 订单Id
+	OrderNumber         string                 `protobuf:"bytes,3,opt,name=order_number,json=orderNumber,proto3" json:"order_number,omitempty"`                            // 订单编号
+	Amount              int64                  `protobuf:"varint,4,opt,name=amount,proto3" json:"amount,omitempty"`                                                        // 金额
+	Phone               string                 `protobuf:"bytes,5,opt,name=phone,proto3" json:"phone,omitempty"`                                                           // 手机号
+	RealName            string                 `protobuf:"bytes,6,opt,name=real_name,json=realName,proto3" json:"real_name,omitempty"`                                     // 姓名
+	RechargeCategoryId  int64                  `protobuf:"varint,7,opt,name=recharge_category_id,json=rechargeCategoryId,proto3" json:"recharge_category_id,omitempty"`    // 充值大类Id
+	RechargeMerchantId  int64                  `protobuf:"varint,8,opt,name=recharge_merchant_id,json=rechargeMerchantId,proto3" json:"recharge_merchant_id,omitempty"`    // 代收商户Id
+	CurrencyCode        string                 `protobuf:"bytes,9,opt,name=currency_code,json=currencyCode,proto3" json:"currency_code,omitempty"`                         // 币种
+	RechargeChannelId   int64                  `protobuf:"varint,10,opt,name=recharge_channel_id,json=rechargeChannelId,proto3" json:"recharge_channel_id,omitempty"`      // 充值通道id
+	Extra               string                 `protobuf:"bytes,11,opt,name=extra,proto3" json:"extra,omitempty"`                                                          // 扩展字段
+	Account             string                 `protobuf:"bytes,12,opt,name=account,proto3" json:"account,omitempty"`                                                      // 用户付款帐号（选填）
+	ConvertCurrencyCode string                 `protobuf:"bytes,13,opt,name=convert_currency_code,json=convertCurrencyCode,proto3" json:"convert_currency_code,omitempty"` // 转换币种（选填）
+	ConvertAmount       string                 `protobuf:"bytes,14,opt,name=convert_amount,json=convertAmount,proto3" json:"convert_amount,omitempty"`                     // 转换币种金额（选填）
+	unknownFields       protoimpl.UnknownFields
+	sizeCache           protoimpl.SizeCache
 }
 
 func (x *PayInReq) Reset() {
@@ -944,6 +946,20 @@ func (x *PayInReq) GetExtra() string {
 func (x *PayInReq) GetAccount() string {
 	if x != nil {
 		return x.Account
+	}
+	return ""
+}
+
+func (x *PayInReq) GetConvertCurrencyCode() string {
+	if x != nil {
+		return x.ConvertCurrencyCode
+	}
+	return ""
+}
+
+func (x *PayInReq) GetConvertAmount() string {
+	if x != nil {
+		return x.ConvertAmount
 	}
 	return ""
 }
@@ -4555,7 +4571,7 @@ var File_proto_finance_v1_finance_proto protoreflect.FileDescriptor
 const file_proto_finance_v1_finance_proto_rawDesc = "" +
 	"\n" +
 	"\x1eproto/finance/v1/finance.proto\x12\n" +
-	"finance.v1\"\x95\x03\n" +
+	"finance.v1\"\xf0\x03\n" +
 	"\bPayInReq\x12\x17\n" +
 	"\auser_id\x18\x01 \x01(\x03R\x06userId\x12\x19\n" +
 	"\border_id\x18\x02 \x01(\tR\aorderId\x12!\n" +
@@ -4569,7 +4585,9 @@ const file_proto_finance_v1_finance_proto_rawDesc = "" +
 	"\x13recharge_channel_id\x18\n" +
 	" \x01(\x03R\x11rechargeChannelId\x12\x14\n" +
 	"\x05extra\x18\v \x01(\tR\x05extra\x12\x18\n" +
-	"\aaccount\x18\f \x01(\tR\aaccount\"\xa7\x01\n" +
+	"\aaccount\x18\f \x01(\tR\aaccount\x122\n" +
+	"\x15convert_currency_code\x18\r \x01(\tR\x13convertCurrencyCode\x12%\n" +
+	"\x0econvert_amount\x18\x0e \x01(\tR\rconvertAmount\"\xa7\x01\n" +
 	"\tPayInResp\x12\x16\n" +
 	"\x06status\x18\x01 \x01(\x03R\x06status\x12\x10\n" +
 	"\x03msg\x18\x02 \x01(\tR\x03msg\x12,\n" +
